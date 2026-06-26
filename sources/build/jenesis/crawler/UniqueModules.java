255d43b9954c97bc0ac499d6389708fdf3b78256
Christian Stein <sormuras@gmail.com>
2026-06-26 08:19:48 +0000

    Add "Unique Modules" generator

---
+++ sources/build/jenesis/crawler/UniqueModules.java
@@ -0,0 +1,57 @@
+package build.jenesis.crawler;
+
+import module java.base;
+
+class UniqueModules {
+    void main() throws Exception {        
+        var root = Path.of("data/modules");
+        try (var stream = Files.walk(root)) {
+            stream
+                .filter(path -> path.getNameCount() > 0)
+                .filter(path -> path.getFileName().toString().equals("modules.tsv"))
+                .map(path -> computeModuleGroupArtifactLine(root, path))
+                .filter(line -> !line.isEmpty())
+                .sorted()
+                .forEach(IO::println);
+        }        
+    }
+
+    static String computeModuleGroupArtifactLine(Path root, Path path) {
+        var joiner = new StringJoiner(".");
+        for (var element : root.relativize(path).getParent()) joiner.add(element.toString());
+        var moduleName = joiner.toString();
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(path);
+            var items = lines.getFirst().split("\t");
+            var mavenGroup = items[1];
+            var mavenGroupAlias = computeMavenGroupAlias(mavenGroup);
+            var mavenArtifact = items[2];
+            var unique = moduleName.startsWith(mavenGroup) || moduleName.startsWith(mavenGroupAlias);
+            if (!unique) {
+            return "";
+            }
+
+            return moduleName + '=' + mavenGroup + ':' + mavenArtifact;
+        } catch (IOException exception) {
+            throw new UncheckedIOException(exception);
+        }
+    }
+
+    static String computeMavenGroupAlias(String group) {
+        return switch (group) {
+            case "com.fasterxml.jackson.core" -> "com.fasterxml.jackson";
+            case "com.github.almasb" -> "com.almasb";
+            case "io.github.openfeign" -> "feign";
+            case "javax.json" -> "java.json";
+            case "net.colesico.framework" -> "colesico.framework";
+            case "org.jetbrains.kotlin" -> "kotlin";
+            case "org.jfxtras" -> "jfxtras";
+            case "org.openjfx" -> "javafx";
+            case "org.ow2.asm" -> "org.objectweb.asm";
+            case "org.projectlombok" -> "lombok";
+            case "org.swimos" -> "swim";
+            default -> group.replace("-", "");
+        };
+    }    
+}
